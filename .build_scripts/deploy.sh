02446c8f8a3e5bce177b03cdda26400739fd2849
Dale Kunce <dale@normalhabit.com>
2017-02-05 22:11:33 -0800

    typo

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -11,7 +11,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; 
   git add .
   git commit -m 'Build successful, build $TRAVIS_BUILD_NUMBER'
   echo "publishing to ${GH_REF} on ${TARGET_BRANCH}"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" ${TARGET_BRANCH} > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:${TARGET_BRANCH} > /dev/null 2>&1
 else
   echo "Not a publishable branch so we're all done here"
 fi
